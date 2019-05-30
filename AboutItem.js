4ebfa518819682d3466044eb892d3e3e0ea25ce2
Arina Prostakova <arina-pro@yandex.ru>
2019-05-31 01:29:42 +0500

    A dumb component

---
+++ AboutItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AboutItem = {
+	return(
+		<View style={flex: 1, alignItems: 'center', justifyContent: 'center'}>
+			<Text> Pokemon App </Text>
+			<Text> ver. 1 </Text>
+		</View>
+		)
+};
+export default AboutItem;
